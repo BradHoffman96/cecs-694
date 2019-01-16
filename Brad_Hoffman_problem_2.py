8dcb84acda04581a46dbf3e7e7f09f72e367ec03
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 15:49:52 -0500

    faulty implementation of the solution. Can't focus enough right now to figure out why this is not working. Going to move to an easier problem

--- Brad_Hoffman_problem_2.py
+++ Brad_Hoffman_problem_2.py
@@ -1,6 +1,18 @@
 
 def main():
-    print("test")
+    string = "()())"
+    solution = [-1]
+    longest = 0
+
+    for index, p in enumerate(string):
+        if p == ")"and len(solution) != 0:
+            pop_index = solution.pop()
+            if index - pop_index > longest:
+                longest = index - pop_index
+        else:
+            solution.append(index)
+
+    print(longest)
 
 if __name__ == "__main__":
     main()
