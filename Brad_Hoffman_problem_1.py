e0782e832425408fa2df345e0b52c2f07296627d
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 10:33:16 -0500

    matrices are being initialized to random values or 0

--- Brad_Hoffman_problem_1.py
+++ Brad_Hoffman_problem_1.py
@@ -1,28 +1,28 @@
-
-matrix_a = [
-        [1, 2],
-        [2, 4]
-    ]
-
-matrix_b = [
-        [3, 2],
-        [1, 4]
-    ]
+import random
 
 def main():
+    col_a = 2
+    col_b = 2
+    row_a = 2
+    row_b = 2
+
+    matrix_a = [[random.randint(0, 5) for i in range(col_a)] for i in range(row_a)]
+    matrix_b = [[random.randint(0, 5) for i in range(col_b)] for i in range(row_b)]
+    matrix_c = [[0 for i in range(col_a * row_b)] for i in range(row_a * col_b)]
+
     print(matrix_a)
     print(matrix_b)
+    print(matrix_c)
 
     #Iterate through the rows of matrix_a
-    for index_row_a, row_a in enumerate(matrix_a):
+    for i in range(row_a):
         #Iterate through the columns of matrix_a
-        for index_col_a, item_a in enumerate(row_a):
+        for j in range(col_a):
             #Iterate through the rows of matrix_b
-            for index_row_b, row_b in enumerate(matrix_b):
+            for k in range(row_b):
                 #Iterate through the columns of matrix_b
-                for index_col_b, item_b in enumerate(row_b):
-                    print(item_a, item_b)
-
+                for l in range(col_a):
+                    print(i + k + 1, j + l + 1)
 
 if __name__ == "__main__":
     main()
