708cc6911b9b2a63a2e3bcecedfb173fab537089
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 14:28:47 -0500

    this is a weird problem due to the indexing. Need to think about how I would handle this one more.

--- Brad_Hoffman_problem_1.py
+++ Brad_Hoffman_problem_1.py
@@ -8,21 +8,26 @@ def main():
 
     matrix_a = [[random.randint(0, 5) for i in range(col_a)] for i in range(row_a)]
     matrix_b = [[random.randint(0, 5) for i in range(col_b)] for i in range(row_b)]
-    matrix_c = [[0 for i in range(col_a * row_b)] for i in range(row_a * col_b)]
+    matrix_c = [[0 for i in range(col_a * col_b)] for i in range(row_a * row_b)]
 
     print(matrix_a)
     print(matrix_b)
     print(matrix_c)
 
     #Iterate through the rows of matrix_a
-    for i in range(row_a):
+    for i in range(0, row_a):
         #Iterate through the columns of matrix_a
-        for j in range(col_a):
+        for k in range(0, row_b):
             #Iterate through the rows of matrix_b
-            for k in range(row_b):
+            for j in range(0, col_a):
                 #Iterate through the columns of matrix_b
-                for l in range(col_a):
-                    print(i + k + 1, j + l + 1)
+                for l in range(0, col_b):
+                    matrix_c[i + l + 1][j + k + 1] = matrix_a[i][j] * matrix_b[k][l]
+
+    print(matrix_c)
+
+    for i in range(col_a * col_b):
+        print(matrix_c[i])
 
 if __name__ == "__main__":
     main()
