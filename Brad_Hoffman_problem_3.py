ded6e2bf3ed2dc26d67bd41a5c8bcf8c309fe806
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 16:29:26 -0500

    I feel like I am going the wrong direction with ordering the k most frequent words by index, so I am going to stop for a bit

--- Brad_Hoffman_problem_3.py
+++ Brad_Hoffman_problem_3.py
@@ -1,5 +1,16 @@
 import operator
 
+def check_index(words_list, current_word, previous_word):
+    current_index = words_list.index(current_word)
+    previous_index = words_list.index(previous_word)
+
+    print(current_index, previous_index)
+
+    if current_index < previous_index:
+        return (current_index, previous_index)
+    else:
+        return (previous_index, current_index)
+
 def main():
 
     #list of words
@@ -19,8 +30,24 @@ def main():
     print(sorted_words)
 
     list_length = len(sorted_words)
+    previous_word = ""
+    previous_count = 0
+    top_k_words = []
     for i in range(1, k + 1):
-        print(sorted_words[list_length - i][0])
+        current_word = sorted_words[list_length - 1][0] 
+        current_count = sorted_words[list_length - 1][1] 
+        
+        if i == 1:
+            previous_word = current_word
+            previous_count = current_count
+        elif previous_count == current_count:
+            #Check the index and add the correct one to the final array
+            first_word, second_word = check_index(words, current_word, previous_word)
+            continue
+
+        top_k_words.append(current_word)
+
+        previous_word = 
 
 if __name__ == "__main__":
     main()
