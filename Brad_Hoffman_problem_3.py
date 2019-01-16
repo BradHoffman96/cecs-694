576ef9d92987bdc28f9a262fe629a7ff1b8073ef
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 16:52:53 -0500

    printing all results if the count is equal

--- Brad_Hoffman_problem_3.py
+++ Brad_Hoffman_problem_3.py
@@ -1,6 +1,38 @@
+from collections import Counter
 
 def main():
-    print("test")
+    words = ["love", "i", "coding", "coding", "i", "love", "Deep", "learning", "i", "love", "coding"]
+    k = 2
+
+    counter = Counter(words)
+    words_by_count = counter.most_common()
+
+    max_count = 0
+    words_list = []
+    for i in range(0, len(words_by_count)):
+        current_word = words_by_count[i][0]
+        current_count = words_by_count[i][1]
+
+        if i == 0:
+            max_count = current_count
+            words_list.append(current_word)
+        elif max_count == current_count:
+            words_list.append(current_word)
+        else:
+            break
+
+    indexes = []
+    for item in words_list:
+        indexes.append(words.index(item))
+
+    sorted_indexes = sorted(indexes)
+
+    final_words = []
+    for i in range(0, len(indexes)):
+        index = sorted_indexes[i]
+        final_words.append(words[index]) 
+
+    print(final_words)
 
 if __name__ == "__main__":
     main()
