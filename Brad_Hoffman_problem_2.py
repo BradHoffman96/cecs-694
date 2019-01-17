c841583364f4e6743c67402c1c19c46311daa74e
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 20:44:08 -0500

    implemented a version that will store the longest valid substring ending at that index. My other solution with the stack did not seem to want to work

--- Brad_Hoffman_problem_2.py
+++ Brad_Hoffman_problem_2.py
@@ -1,18 +1,39 @@
 
-def main():
-    string = "()())"
-    solution = [-1]
+def stack_version(string):
+    last = -1
     longest = 0
+    stack = []
 
     for index, p in enumerate(string):
-        if p == ")"and len(solution) != 0:
-            pop_index = solution.pop()
-            if index - pop_index > longest:
-                longest = index - pop_index
+        if p == "(":
+            stack.append(index)
         else:
-            solution.append(index)
+            stack_len = len(stack)
+            if stack_len == 0:
+                last = index
+            else:
+                item = stack.pop()
+                if stack_len == 0:
+                    longest = max(longest, index - last)
+                else:
+                    longest = max(longest, index - item)
 
-    print(longest)
+    return longest
+
+def main():
+    string = "()())"
+
+    print(stack_version(string))
+
+    longest = [0] * len(string)
+    for i in range(1, len(string)):
+        if string[i] == ')':
+            # Index where matching ( would have to be, if it exists.
+            j = i - longest[i-1] - 1
+            if j >= 0 and string[j] == '(':
+                longest[i] = 1 + i - j + longest[j-1]
+    
+    print(max(longest))
 
 if __name__ == "__main__":
     main()
