d5e504f9ed7c4ef97b4abe15fc988a6db02e8281
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-01-16 22:46:18 -0500

    it seems that watching videos to understand the problem really helps me implement a solution. Problem #2 finished

--- Brad_Hoffman_problem_2.py
+++ Brad_Hoffman_problem_2.py
@@ -1,39 +1,20 @@
+def main():
+    string = "())"
 
-def stack_version(string):
-    last = -1
+    stack = [-1]
     longest = 0
-    stack = []
 
     for index, p in enumerate(string):
         if p == "(":
             stack.append(index)
         else:
-            stack_len = len(stack)
-            if stack_len == 0:
-                last = index
+            stack.pop()
+            if len(stack) == 0:
+                stack.append(index)
             else:
-                item = stack.pop()
-                if stack_len == 0:
-                    longest = max(longest, index - last)
-                else:
-                    longest = max(longest, index - item)
+                longest = max(longest, index - stack[len(stack) - 1])
 
-    return longest
-
-def main():
-    string = "()())"
-
-    print(stack_version(string))
-
-    longest = [0] * len(string)
-    for i in range(1, len(string)):
-        if string[i] == ')':
-            # Index where matching ( would have to be, if it exists.
-            j = i - longest[i-1] - 1
-            if j >= 0 and string[j] == '(':
-                longest[i] = 1 + i - j + longest[j-1]
-    
-    print(max(longest))
+    print(longest)
 
 if __name__ == "__main__":
     main()
